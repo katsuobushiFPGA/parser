1747912b4ea387010746a486a6875da267b57b80
katsuobushi <katsuobushiFPGA>
2014-05-27 10:04:43 +0900

    v1.3

--- JavaCCParserTest/src/Rei.java
+++ JavaCCParserTest/src/Rei.java
@@ -4,12 +4,7 @@ class Rei {
     public static void main(String[] arg) throws ParseException {
 	final Reader r = new InputStreamReader(System.in);
 	final Parser parser = new Parser(r);
-	final Node tree = parser.start();
-	if(tree !=null){
-	    tree.show();
-	    System.out.println();
-	    tree.rpn();
-	    System.out.println();
+	final int tree = parser.start();
+	  System.out.println(tree);
 	}
-    }
 }
